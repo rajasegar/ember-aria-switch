94361e8c83178428e611a6c4cc7a105309176a09
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-06-02 11:27:33 +0530

    fix: lint errors with new lint rules

--- addon/components/aria-switch.js
+++ addon/components/aria-switch.js
@@ -1,36 +1,36 @@
-import { reads } from '@ember/object/computed';
-import { computed } from '@ember/object';
-import Component from '@ember/component';
-import layout from '../templates/components/aria-switch';
+import { reads } from "@ember/object/computed";
+import { computed } from "@ember/object";
+import Component from "@ember/component";
+import layout from "../templates/components/aria-switch";
 
 export default Component.extend({
   layout,
   checked: false,
-  dataAction: 'aria-switch',
-  offLabel: 'Off',
-  onLabel: 'On',
-  role: 'switch',
-  type: 'button',
-  tagName: 'button',
+  dataAction: "aria-switch",
+  offLabel: "Off",
+  onLabel: "On",
+  role: "switch",
+  type: "button",
+  tagName: "button",
   attributeBindings: [
-    'ariaChecked:aria-checked',
-    'ariaLabel:aria-label',
-    'ariaLabelledBy:aria-labelledby',
-    'dataAction:data-action',
-    'dataKeepDisabled:data-keep-disabled',
-    'disabled',
-    'role',
-    'type',
+    "ariaChecked:aria-checked",
+    "ariaLabel:aria-label",
+    "ariaLabelledBy:aria-labelledby",
+    "dataAction:data-action",
+    "dataKeepDisabled:data-keep-disabled",
+    "disabled",
+    "role",
+    "type",
   ],
-  ariaLabel: reads('label'),
-  dataKeepDisabled: reads('disabled'),
-  ariaChecked: computed('checked', function() {
-    return this.get('checked') ? "true" : "false";
+  ariaLabel: reads("label"),
+  dataKeepDisabled: reads("disabled"),
+  ariaChecked: computed("checked", function () {
+    return this.checked ? "true" : "false";
   }),
   click() {
-    this.toggleProperty('checked');
-    if(this.get('onToggle')) {
-      this.get('onToggle')(this.get('checked'));
+    this.toggleProperty("checked");
+    if (this.onToggle) {
+      this.onToggle(this.checked);
     }
-  }
+  },
 });
